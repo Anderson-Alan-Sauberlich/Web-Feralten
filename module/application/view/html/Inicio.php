cb397d50cadfa312e439b649e57230dbc73bb4a9
Anderson-Alan-Sauberlich <andersonalansauberlich@outlook.com>
2017-11-10 12:04:49 -0200

    Adicionado Imagem de Peças no Head da Pagina: Inicio.

--- module/application/view/html/Inicio.php
+++ module/application/view/html/Inicio.php
@@ -10,18 +10,14 @@
     	<?php include_once RAIZ.'/module/application/view/html/layout/header/Cabecalho.php'; ?>
     </header>
     <section class="ui container" role="main">
+    	<img src="/resources/img/pecas_head.jpg" position="center" class="img-responsive headIMG " />
     	<form id="searschform" class="form-horizontal" name="searschform" action="/pecas/resultados/" method="get" role="form">
         	<?php View_Inicio::Incluir_Menu_Pesquisa(); ?>
         </form>
-        
-        
-            
-                <img src="/resources/img/fundo_feralten_s.jpeg" position="center" class="img-responsive centerIMG " />
-            	<BR/>
-            	<BR/>
-            	<BR/>
-        
-        
+        <img src="/resources/img/fundo_feralten_s.jpeg" position="center" class="img-responsive centerIMG " />
+        <BR/>
+        <BR/>
+        <BR/>
     </section>
     <footer>
         <?php include_once RAIZ.'/module/application/view/html/layout/footer/Rodape.php'; ?>
