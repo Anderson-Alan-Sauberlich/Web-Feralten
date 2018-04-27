ad83f95337bdd88fee4f14dfb750455b4623f37e
Anderson-Alan-Sauberlich <andersonalansauberlich@outlook.com>
2018-04-27 20:59:31 -0300

    Alterado gif loader. Invertido forms Modal Solicitar Orçamentos

--- Module/Application/View/HTML/Layout/Modal/Solicitar_Orcamento.php
+++ Module/Application/View/HTML/Layout/Modal/Solicitar_Orcamento.php
@@ -14,10 +14,35 @@
 </div>
 <div id="mdl_orcamento" class="ui modal">
 	<i class="close icon"></i>
-	<div class="content">
+	<div class="scrolling content">
 		<?php if (!View_Solicitar_Orcamento::Verificar_Logado()) { ?>
 			<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit&hl=pt" async defer></script>
         	<div id="div_autenticacao" class="ui two column relaxed stackable internally celled grid">
+        		<div class="column">
+            		<div id="form_login" class="ui equal width form">
+                    	<h2 class="ui dividing header">Entrar com sua conta</h2>
+                    	<div class="field">
+                           	<label for="login_email">Seu E-Mail:</label>
+                           	<input id="login_email" placeholder="E-Mail" type="email">
+                    	</div>
+                    	<div class="field">
+                           	<label for="login_senha">Sua senha:</label>
+                           	<input id="login_senha" placeholder="Senha" type="password">
+                    	</div>
+                        <div class="field">
+                        	<div class="ui checked checkbox">
+                            	<input tabindex="0" checked="checked" id="login_manter" class="hidden" type="checkbox">
+                            	<label for="login_manter">Mantenha-me Conectado</label>
+                        	</div>
+                        </div>
+                        <div id="login_msg" class="ui hidden message">
+                        	<i class="close icon"></i>
+                        	<div id="login_msg_header" class="header"></div>
+                        	<ul id="login_msg_list" class="list"></ul>
+                        </div>
+                    	<button onclick="logarUsuario();" class="ui red big fluid button" type="submit">Entrar</button>
+                    </div>
+                </div>
         		<div class="column">
                     <div id="form_cadastro" class="ui equal width form">
                     	<h2 class="ui dividing header">Criar uma conta</h2>
@@ -48,31 +73,6 @@
                         	<ul id="cadastro_msg_list" class="list"></ul>
                         </div>
                     	<button onclick="cadastrarUsuario()" class="ui green big fluid button" type="submit">Criar Conta</button>
-                    </div>
-                </div>
-        		<div class="column">
-            		<div id="form_login" class="ui equal width form">
-                    	<h2 class="ui dividing header">Entrar com sua conta</h2>
-                    	<div class="field">
-                           	<label for="login_email">Seu E-Mail:</label>
-                           	<input id="login_email" placeholder="E-Mail" type="email">
-                    	</div>
-                    	<div class="field">
-                           	<label for="login_senha">Sua senha:</label>
-                           	<input id="login_senha" placeholder="Senha" type="password">
-                    	</div>
-                        <div class="field">
-                        	<div class="ui checked checkbox">
-                            	<input tabindex="0" checked="checked" id="login_manter" class="hidden" type="checkbox">
-                            	<label for="login_manter">Mantenha-me Conectado</label>
-                        	</div>
-                        </div>
-                        <div id="login_msg" class="ui hidden message">
-                        	<i class="close icon"></i>
-                        	<div id="login_msg_header" class="header"></div>
-                        	<ul id="login_msg_list" class="list"></ul>
-                        </div>
-                    	<button onclick="logarUsuario();" class="ui red big fluid button" type="submit">Entrar</button>
                     </div>
                 </div>
             </div>
