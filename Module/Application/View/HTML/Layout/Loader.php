59fb4fbe11df34add5c62c06346f7f62253a3bf4
Anderson-Alan-Sauberlich <andersonalansauberlich@outlook.com>
2018-04-28 11:51:45 -0300

    Alterado nome arquivo .gif para loading

--- Module/Application/View/HTML/Layout/Loader.php
+++ Module/Application/View/HTML/Layout/Loader.php
@@ -1,4 +1,4 @@
 <div id="loader">
-    <img src="/resources/img/loader.gif" class="centerIMG" alt="Carregando..." height="75" width="75">
+    <img src="/resources/img/loading.gif" class="centerIMG" alt="Carregando..." height="75" width="75">
 </div>
 <script type="text/javascript" src="/application/js/layout/loader.js"></script>/
